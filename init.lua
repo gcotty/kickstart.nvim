ec76352ae078170b1434eecc151cc4624a7c73af
grant cottingham <cottingham.glc@gmail.com>
2026-08-20 00:41:19 -0400

    gitsigns plugin

--- init.lua
+++ init.lua
@@ -940,7 +940,7 @@ do
   -- require 'kickstart.plugins.lint'
   -- require 'kickstart.plugins.autopairs'
   -- require 'kickstart.plugins.neo-tree'
-  -- require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
+  require 'kickstart.plugins.gitsigns'
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --
